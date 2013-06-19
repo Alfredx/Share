a824fd9f5b4f40127a45435d7228aea0bf2c8ff5
Andriy Lin <lxk19921205@gmail.com>
2013-06-19 10:51:52 +0800

    separate data structures into models.js

---
+++ routes/models.js
@@ -0,0 +1,268 @@
+/*
+ * Define the data models used in this project.
+ */
+
+/**
+ * Containing all the information of a user.
+ */
+var UserData = function() {
+    /**
+     * The socket id assigned.
+     * @type {String}
+     */
+    this.id = null;
+    /**
+     * Socket object used in socket.io.
+     * @type {Object}
+     */
+    this.socket = null;
+
+    /**
+     * *.*.*.*
+     * @type {String}
+     */
+    this.ip = null;
+    /**
+     * e.g. 80
+     * @type {Number}
+     */
+    this.port = null;
+
+    /**
+     * The longitude of geolocation.
+     * @type {Number}
+     */
+    this.geoLongitude = null;
+    /**
+     * The latitude of geolocation.
+     * @type {Number}
+     */
+    this.geoLatitude = null;
+    /**
+     * The accuracy of geolocation.
+     * @type {Number}
+     */
+    this.geoAccuracy = null;
+
+    /**
+     * The name of the file user wants to send.
+     * Or null if user just want to receive.
+     * @type {String}
+     */
+    this.fileName = null;
+    /**
+     * The size of the file user wants to send.
+     * Or null if user just want to receive.
+     * @type {Number}
+     */
+    this.fileSize = null;
+};
+
+
+/**
+ * The timeout for making a pair. In milliseconds.
+ * @type {Number}
+ */
+var TIMEOUT = 1500;
+
+/**
+ * A data structure that stores users.
+ * Expecially for saving users that are waiting to be paired.
+ */
+var Users = function() {
+    /**
+     * Storing all the users.
+     * 'user/socket id' => UserData
+     * @type {Object}
+     */
+    this._store = {};
+
+    /**
+     * Remove the user's data in store.
+     * @param  {Number} userID The ID of the user to be removed.
+     */
+    this.clear = function(userID) {
+        delete this._store[userID];
+    };
+
+    /**
+     * Add the user into this store for TIMEOUT milliseconds.
+     * After it expires, it will be deleted and the callback will be called.
+     * @param  {Object} user       The user to be added.
+     * @param  {Function} callback To be called when it expires. The user will be passed.
+     */
+    this.addTillExpire = function(user, callback) {
+        this._store[user.id] = user;
+
+        var theID = user.id;
+        var store = this._store;
+        setTimeout(function() {
+            if (theID in store) {
+                var u = store[theID];
+                callback(u);
+                delete store[theID];
+            }
+        }, TIMEOUT);
+    };
+
+    /**
+     * Given a baseUser, find the nearest user.
+     * @param  {Object} baseUser The user to be compared.
+     * @return {Object}          The nearest user.
+     */
+    this.pickUpon = function(baseUser) {
+        for (var uid in this._store) {
+            // TODO: just pick one...
+            return this._store[uid];
+        }
+    };
+
+    /**
+     * Get all the users' IDs.
+     * @return {Object} An array of the ids.
+     */
+    this.userIDs = function() {
+        return Object.keys(this._store);
+    };
+};
+
+
+/**
+ * Containing the necessary for a paired connection.
+ */
+var Connection = function() {
+    /**
+     * The user that sends files.
+     * @type {Object}
+     */
+    this.sender = null;
+    /**
+     * Whether sender has confirmed to send.
+     * @type {Boolean}
+     */
+    this.senderConfirmed = false;
+
+    /**
+     * The user that receives files.
+     * @type {Object}
+     */
+    this.receiver = null;
+    /**
+     * Whether receiver has confirmd to receive.
+     * @type {Boolean}
+     */
+    this.receiverConfirmed = false;
+
+    /**
+     * The status of this connection.
+     * Can be:
+     *     INIT: just inited
+     *     SENDING: sender is sending
+     *     RECEIVING: receiver is receiving
+     * @type {String}
+     */
+    this.status = "INIT";
+};
+
+
+/**
+ * Storing all connections.
+ */
+var Pairs = function() {
+    /**
+     * The data structure that stores all the connections.
+     * 'senderID' => Connection
+     * 'receiverID' => the same Connection
+     * @type {Object}
+     */
+    this._connections = {};
+
+    /**
+     * Remove the connection relative to this user.
+     * @param  {Number} userID The ID of a user in connection.
+     */
+    this.clear = function(userID) {
+        if (!(userID in this._connections)) {
+            return;
+        }
+
+        var con = this._connections[userID];
+        var sender = con.sender;
+        var receiver = con.receiver;
+        // TODO: remove the socket thing.
+        if (userID === sender.id) {
+            // tell receiver
+            receiver.socket.emit('betrayedSending', {
+                'partnerID': sender.id
+            });
+        } else {
+            // tell sender
+            sender.socket.emit('betrayedReceiving', {
+                'partnerID': receiver.id
+            });
+        }
+
+        delete this._connections[sender.id];
+        delete this._connections[receiver.id];
+    };
+
+    /**
+     * Add a new connection after pairing.
+     * @param  {Object} sender   The user that sends files.
+     * @param  {Object} receiver The user that receives files.
+     */
+    this.add = function(sender, receiver) {
+        this.clear(sender.id);
+        this.clear(receiver.id);
+
+        var con = new Connection();
+        con.sender = sender;
+        con.receiver = receiver;
+
+        this._connections[sender.id] = con;
+        this._connections[receiver.id] = con;
+    };
+
+    /**
+     * A user confirms to share files.
+     * @param  {Number} myID      The ID of the user that confirms.
+     * @param  {Number} partnerID The ID of the other user.
+     */
+    this.confirm = function(myID, partnerID) {
+        if (!(myID in this._connections) || !(partnerID in this._connections)) {
+            return;
+        }
+
+        var con = this._connections[myID];
+        var sender = con.sender;
+        var receiver = con.receiver;
+
+        if (sender.id === myID) {
+            // this user is the sender
+            con.senderConfirmed = true;
+        } else {
+            // this user is the receiver
+            con.receiverConfirmed = true;
+        }
+
+        if (con.senderConfirmed && con.receiverConfirmed) {
+            // both confirmed, real start
+            sender.socket.emit('startSending', {
+                'senderID': sender.id,
+                'receiverID': receiver.id
+            });
+            receiver.socket.emit('startSending', {
+                'senderID': sender.id,
+                'receiverID': receiver.id
+            });
+            con.status = "SENDING";
+        }
+    };
+};
+
+
+// exports
+exports.UserData = UserData;
+exports.Users = Users;
+exports.Connection = Connection;
+exports.Pairs = Pairs;
