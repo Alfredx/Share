e93757f412eaced63af75db0d3d0c5b3a70a7302
Alfredx <alfredcameoutwithnonames@gmail.com>
2013-08-05 13:13:49 +0800

    made some changes on name is null or didnt give a new name

--- public/javascripts/userLogic.js
+++ public/javascripts/userLogic.js
@@ -1027,6 +1027,8 @@ var onload = function(socket){
             showMessage('Connected, id is ' + data);
         }
         var name = prompt("To receive better experience\nPlease tell us your name:","(your name here)");
+        if(name === null || name == "(your name here)");
+            name = "user "+id;
         socket.emit('iam', {
             'id' : id,
             'name':name,
